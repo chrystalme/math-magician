8df74250981e739662e6f197e44757a8c5baa6dc
chrystalme <afam_ifeanyi@live.com>
2021-07-06 18:50:09 +0300

    Update calculate method

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,6 +1,12 @@
 import operate from './operate';
 
 const calculate = (calculatorData, buttonName) => {
-  
+  const { total, next, operation } = calculatorData;
+  const result = calculatorData;
+  if (['+', '-', 'x', '÷', '%'].includes(buttonName) && buttonName === operation) {
+    if (total && next) {
+      result.total = operate(total, next, buttonName);
+    }
+  }
 };
 export default calculate;
