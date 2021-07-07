eda400725fe21cd1f261a50386b3dee2c52b733b
chrystalme <afam_ifeanyi@live.com>
2021-07-07 11:48:57 +0300

    Update app.js to include calculate

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Display from './display';
 import ButtonPanel from './buttonPanel';
+import calculate from '../logic/calculate';
 import '../App.css';
 
 function App() {
+  calculate();
   return (
 
     <>
--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -24,9 +24,34 @@ const calculate = (calculatorData, buttonName) => {
     }
   }
 
+  if (buttonName === '+/-') {
+    if (total && !next) {
+      result.total = operate(total, -1, 'x');
+    }
+    if (next) {
+      result.total = operate(next, -1, 'x');
+    }
+  }
+
   if (operators.includes(buttonName) /* && buttonName === operation */) {
     if (total && next) {
       result.total = operate(total, next, buttonName);
+    }
+  }
+
+  if (numbers.test(buttonName)) {
+    if (!total || total === '0') {
+      result.total = buttonName;
+    }
+    if (total && total !== '0' && !next && !operation) {
+      result.total += buttonName;
+      if (buttonName === '0' && total === '0') result.total = total;
+    }
+    if (total && operation && (next === '0' || !next)) {
+      result.next = buttonName;
+    }
+    if (total && next && operation && next !== '0') {
+      result.next += buttonName;
     }
   }
   return result;
