e01cbdbaf83b8837e371a5bff1ee7ddcd2c639bc
chrystalme <afam_ifeanyi@live.com>
2021-07-19 15:44:20 +0300

    Add test for calculate

---
+++ src/__tests__/calculate-test.js
@@ -0,0 +1,32 @@
+import calculate from '../logic/calculate';
+
+describe('Calculate logic', () => {
+  test('AC button works as expected', () => {
+    const result = calculate({ total: '3', next: '.', operation: '' }, 'AC');
+    expect(result.total).toBe(undefined);
+  });
+  test('+ button works as expected', () => {
+    const result = calculate({ total: '3', next: '7', operation: '+' }, '+');
+    expect(result.total).toEqual('10');
+  });
+  test('- button works as expected', () => {
+    const result = calculate({ total: '7', next: '3', operation: '-' }, '=');
+    expect(result.total).toEqual('4');
+  });
+  test('% button works as expected', () => {
+    const result = calculate({ total: '3', next: '', operation: '' }, '%');
+    expect(result.total).toEqual('0.03');
+  });
+  test('+/- button works as expected', () => {
+    const result = calculate({ total: '3', next: '', operation: '' }, '+/-');
+    expect(result.total).toEqual('-3');
+  });
+  test('= button works as expected', () => {
+    const result = calculate({ total: '3', next: '7', operation: '+' }, '=');
+    expect(result.total).toEqual('10');
+  });
+  test('. button works as expected', () => {
+    const result = calculate({ total: '3', next: '', operation: '' }, '.');
+    expect(result.total).toEqual('3.');
+  });
+});
