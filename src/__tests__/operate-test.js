68544010540de9b087dffcdbef09087d93917bc8
chrystalme <afam_ifeanyi@live.com>
2021-07-19 15:19:35 +0300

    Complete operate tests

--- src/__tests__/operate-test.js
+++ src/__tests__/operate-test.js
@@ -3,8 +3,20 @@ import operate from '../logic/operate';
 const x = 10;
 const y = 34;
 describe('operate', () => {
-  test('sum of two x and y', () => {
+  test('sum of x and y', () => {
     const result = operate(x, y, '+');
     expect(result).toEqual('44');
+  });
+  test('subtract x from y', () => {
+    const result = operate(x, y, '-');
+    expect(result).toEqual('-24');
+  });
+  test('divide x and y', () => {
+    const result = operate(y, x, '÷');
+    expect(result).toEqual('3.4');
+  });
+  test('multiply x and y', () => {
+    const result = operate(x, y, 'x');
+    expect(result).toEqual('340');
   });
 });
