51af762af9d793c700379c5af8a739d104d44e8d
chrystalme <afam_ifeanyi@live.com>
2021-07-21 10:48:11 +0300

    Update quotes with clean code

--- src/__tests__/app-test.js
+++ src/__tests__/app-test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import App from '../components/app';
 import Routes from '../components/routes';
 
--- src/__tests__/home-test.js
+++ src/__tests__/home-test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Routes from '../components/routes';
 import Home from '../components/home';
 
@@ -7,8 +8,7 @@ describe('Home', () => {
   test('Text match Home page test', () => {
     const { getByText } = render(<Routes><Home /></Routes>);
     const text = getByText('Math-magician');
-    // eslint-disable-next-line no-unused-expressions
-    expect(text).toBeInTheDocument;
+    expect(text).toBeInTheDocument();
   });
   test('Test Home page snapshot', () => {
     const { getByText } = render(<Routes><Home /></Routes>);
